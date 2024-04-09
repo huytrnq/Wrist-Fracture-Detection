c910c0ea93db7ae9d2163704747d04729056fa91
huytrnq <huytrq.em@gmail.com>
2024-04-09 11:02:19 +0700

    huy exps

---
+++ utils/intensity_transforms.py
@@ -0,0 +1,34 @@
+import cv2
+
+class IntensityTransformation:
+    def __init__(self) -> None:
+        pass
+    
+    def load_img(self, img_path):
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        assert img is not None, "file could not be read, check with os.path.exists()"
+        return img
+        
+    def linear_strerching(self, img):
+        img = self.load_img(img) if isinstance(img, str) else img
+        min_intensity = img.min()
+        max_intensity = img.max()
+        img = (img - min_intensity) / (max_intensity - min_intensity) * 255
+        return img
+    
+    def histogram_equalization(self, img):
+        img = self.load_img(img) if isinstance(img, str) else img
+        img = cv2.equalizeHist(img)
+        return img
+
+
+if __name__ == '__main__':
+    img_path = '/Users/huytrq/Downloads/Compress/Extracted/folder_structure/supervisely/wrist/img/0031_1007172623_02_WRI-R1_M009.png'
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    assert img is not None, "file could not be read, check with os.path.exists()"
+    intensity = IntensityTransformation()
+    linear_streched_img = intensity.linear_strerching(img)
+    his_eq_img = intensity.histogram_equalization(img)
+    cv2.imshow('Linear Stretching', his_eq_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
---
+++ utils/thresholding.py
@@ -0,0 +1,59 @@
+import cv2
+import matplotlib.pyplot as plt
+
+class ImageThresholding:
+    def __init__(self, blur=False, blur_kernel=5):
+        self.blur = blur
+        self.blur_kernel = blur_kernel
+        self.img = None
+        self.current_img_path = None
+    
+    def load_img(self, img_path):
+        if img_path == self.current_img_path:
+            return self.img
+        self.current_img_path = img_path
+        self.img = cv2.imread(img_path, 0)
+        self.img = cv2.medianBlur(self.img, self.blur_kernel) if self.blur else self.img
+        return self.img
+
+    def global_thresholding(self, img, threshold_value=100, max_value=255):
+        self.img = self.load_img(img) if type(img) is str else img
+        ret, th1 = cv2.threshold(self.img, threshold_value, max_value, cv2.THRESH_BINARY)
+        return th1
+
+    def adaptive_mean_thresholding(self, img, max_value=255, block_size=11, C=2):
+        self.img = self.load_img(img) if type(img) is str else img
+        th2 = cv2.adaptiveThreshold(self.img, max_value, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, block_size, C)
+        return th2
+
+    def adaptive_gaussian_thresholding(self, img, max_value=255, block_size=11, C=2):
+        self.img = self.load_img(img) if type(img) is str else img
+        th3 = cv2.adaptiveThreshold(self.img, max_value, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, block_size, C)
+        return th3
+    
+    def otsu_thresholding(self, img, max_value=255):
+        self.img = self.load_img(img) if type(img) is str else img
+        _, th1 = cv2.threshold(self.img, 0, max_value, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        return th1
+
+    def triangle_thresholding(self, img, max_value=255):
+        self.img = self.load_img(img) if type(img) is str else img
+        _, th1 = cv2.threshold(self.img, 0, max_value, cv2.THRESH_BINARY+cv2.THRESH_TRIANGLE)
+        return th1
+    
+    def display_images(self, images, titles):
+        for i in range(len(images)):
+            plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
+            plt.title(titles[i])
+            plt.xticks([]),plt.yticks([])
+        plt.show()
+        
+if __name__ == '__main__':
+    image_path = '/Users/huytrq/Downloads/Compress/Extracted/folder_structure/supervisely/wrist/img/0029_0571570953_01_WRI-L1_M008.png'
+    image = ImageThresholding()
+    gt_image = image.global_thresholding(image_path)
+    am_image = image.adaptive_mean_thresholding(image_path)
+    ag_image = image.adaptive_gaussian_thresholding(image_path)
+    ot_image = image.otsu_thresholding(image_path)
+    tr_image = image.triangle_thresholding(image_path)
+    image.display_images([image.img, gt_image, am_image, ag_image, ot_image, tr_image], ['Original Image', 'Global Thresholding (v=100)', 'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding', 'Otsu Thresholding', 'Triangle Thresholding'])    
